47729ec674df00266ce21b3e7b1a10181e645fb0
Fernando Luis <fernando@Fernandos-MacBook-Pro.local>
2024-03-28 20:23:52 -0300

    codigo feitos em aula 28/03

--- src/pages/login-page/login-page.component.ts
+++ src/pages/login-page/login-page.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [NavComponent, MatFormFieldModule],
+  imports: [
+    NavComponent,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  loginForm!: FormGroup;
+  //emailControl = new FormControl('valor padrao');
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', [Validators.required]),
+    });
+  }
+
+  autenticar() {
+    // console.log('click botao');
+    // console.log(this.loginForm.get('email')?.value);
+    console.log(this.loginForm.value);
+    if (this.loginForm.valid) {
+      const { email, senha } = this.loginForm.value;
+      console.log('teste', email, senha);
+    }
+    return false;
+  }
+}
